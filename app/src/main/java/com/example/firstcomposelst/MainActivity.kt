38e079746328ed56200780544f9f20ecf7dc3de0
j.shirkhani <j.shirkhani@fanap@.ir>
2022-11-20 00:31:12 +0330

    initialization

---
+++ app/src/main/java/com/example/firstcomposelst/MainActivity.kt
@@ -0,0 +1,63 @@
+package com.example.firstcomposelst
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.firstcomposelst.ui.theme.FirstComposeLstTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            FirstComposeLstTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    CreateBizCard()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun CreateBizCard() {
+
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+    ) {
+
+        Card(
+            modifier = Modifier
+                .width(200.dp)
+                .height(390.dp),
+            shape = RoundedCornerShape(corner = CornerSize(15.dp)), elevation = 4.dp
+        ) {
+
+        }
+
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    FirstComposeLstTheme {
+        CreateBizCard()
+    }
+}
