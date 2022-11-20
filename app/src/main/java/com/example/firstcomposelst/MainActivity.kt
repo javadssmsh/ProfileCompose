9e8deb191ebde2ccaf9db577a5d543e1e4480b69
j.shirkhani <j.shirkhani@fanap@.ir>
2022-11-20 19:11:48 +0330

    add profile image

--- app/src/main/java/com/example/firstcomposelst/MainActivity.kt
+++ app/src/main/java/com/example/firstcomposelst/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.example.firstcomposelst
 
+import android.icu.number.Scale
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
@@ -13,6 +17,8 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.firstcomposelst.ui.theme.FirstComposeLstTheme
@@ -51,6 +57,25 @@ fun CreateBizCard() {
             backgroundColor = Color.White,
             elevation = 4.dp
         ) {
+
+            androidx.compose.material.Surface(
+                modifier = Modifier
+                    .size(150.dp)
+                    .padding(5.dp),
+                shape = CircleShape,
+                border = BorderStroke(0.5.dp, Color.LightGray),
+                elevation = 5.dp,
+                color = MaterialTheme.colors.surface.copy(alpha = 0.5f)
+            ) {
+
+                Image(
+                    painter = painterResource(id = R.drawable.profile_image),
+                    contentDescription = "profile image ",
+                    modifier = Modifier.size(135.dp),
+                    contentScale = ContentScale.Crop
+                )
+
+            }
 
         }
 
