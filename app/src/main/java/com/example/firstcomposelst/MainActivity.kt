393c41b9dafb991c0ee14c060611f0f62c9318bf
j.shirkhani <j.shirkhani@fanap@.ir>
2022-11-25 14:52:59 +0330

    add card info section

--- app/src/main/java/com/example/firstcomposelst/MainActivity.kt
+++ app/src/main/java/com/example/firstcomposelst/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.firstcomposelst
 
-import android.icu.number.Scale
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -10,10 +9,7 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -65,11 +61,36 @@ fun CreateBizCard() {
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
                 CreateImageProfile()
+                Divider()
+                CreateInfo()
             }
 
 
         }
 
+    }
+}
+
+@Composable
+private fun CreateInfo() {
+    Column(modifier = Modifier.padding(5.dp)) {
+        Text(
+            text = "Javad",
+            style = MaterialTheme.typography.h4,
+            color = MaterialTheme.colors.primaryVariant
+        )
+
+        Text(
+            text = "Android Developer",
+            modifier = Modifier.padding(3.dp),
+            style = MaterialTheme.typography.subtitle1
+        )
+
+        Text(
+            text = "javadssmh@gmail.com",
+            modifier = Modifier.padding(3.dp),
+            style = MaterialTheme.typography.subtitle1
+        )
     }
 }
 
